b647e2521490b0c39b4159579647f6dacf6ac8e1
Lazu_ve01@student.itstep.org <Lazu_ve01@student.itstep.org>
2024-06-25 11:47:55 +0100

    add contact adaptation

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -50,54 +50,6 @@ const Portfolio = () => {
                         </p>
                         <p className={styles["project-tech"]}>Tech used: React, Redux, React Router DOM, Axios, PayPal API and Google API</p>
                     </div>  
-
-                    <div className={styles["project-card"]}>
-                        <img className={styles["project-image"]} src={blahoFilm} alt="" />
-
-                        <div className={styles["project-header"]}>
-                            <p className={styles["project-name"]}>BlahoFilm</p>
-                            <div>
-                                <FontAwesomeIcon icon={faGithub} onClick={blahofilmGitHub} />
-                                <FontAwesomeIcon icon={faGlobe} onClick={blahofilmDemo} />
-                            </div>
-                        </div>
-                        <p className={styles["project-info"]}>
-                            BlahoFilm is an innovative streaming service designed to provide
-                            users with the best experience of watching movies, series, cartoons
-                            and anime. This project was developed as part of my thesis and is
-                            now an important part of my portfolio.
-                        </p>
-                        <p className={styles["project-info"]}>
-                            In this project, I was fully involved in the development of the
-                            client part, namely project architecture, page layout, development of
-                            website functionality, integration of requests to the backend, and much more.
-                        </p>
-                        <p className={styles["project-tech"]}>Tech used: React, Redux, React Router DOM, Axios, PayPal API and Google API</p>
-                    </div>  
-
-                    <div className={styles["project-card"]}>
-                        <img className={styles["project-image"]} src={blahoFilm} alt="" />
-
-                        <div className={styles["project-header"]}>
-                            <p className={styles["project-name"]}>BlahoFilm</p>
-                            <div>
-                                <FontAwesomeIcon icon={faGithub} onClick={blahofilmGitHub} />
-                                <FontAwesomeIcon icon={faGlobe} onClick={blahofilmDemo} />
-                            </div>
-                        </div>
-                        <p className={styles["project-info"]}>
-                            BlahoFilm is an innovative streaming service designed to provide
-                            users with the best experience of watching movies, series, cartoons
-                            and anime. This project was developed as part of my thesis and is
-                            now an important part of my portfolio.
-                        </p>
-                        <p className={styles["project-info"]}>
-                            In this project, I was fully involved in the development of the
-                            client part, namely project architecture, page layout, development of
-                            website functionality, integration of requests to the backend, and much more.
-                        </p>
-                        <p className={styles["project-tech"]}>Tech used: React, Redux, React Router DOM, Axios, PayPal API and Google API</p>
-                    </div>  
                     
                 </div>
 
